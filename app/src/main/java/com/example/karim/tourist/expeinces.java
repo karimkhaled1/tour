4e3ad28317574aa8e9c1a554fe198253d6e307e8
karimkhaled1 <30834525+karimkhaled1@users.noreply.github.com>
2018-06-27 21:48:39 +0200

    w

--- app/src/main/java/com/example/karim/tourist/expeinces.java
+++ app/src/main/java/com/example/karim/tourist/expeinces.java
@@ -1,13 +1,17 @@
 package com.example.karim.tourist;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.HorizontalScrollView;
@@ -17,12 +21,17 @@ import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 //get all data
@@ -44,16 +53,19 @@ public class expeinces extends AppCompatActivity {
 
     private void getData() {
         // get all data
-        final DatabaseReference database = FirebaseDatabase.getInstance().getReference("exp").child("alex");
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference("exp").child("alex").child("food");
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.e("print",dataSnapshot.toString());
+                ArrayList<proxyItem> proxyItems =new ArrayList<>();
+                ArrayList<String> keys =new ArrayList<>();
                  for(DataSnapshot snap:dataSnapshot.getChildren()){
-                  x cat=snap.getValue(x.class);
-                     showCatItems(cat,database);
+                  proxyItems.add(snap.getValue(proxyItem.class));
+                  keys.add(snap.getKey());
+
                      Log.e("print",snap.toString());
                  }
+                showCatItems(proxyItems,database,keys,"food");
             }
 
             @Override
@@ -64,12 +76,60 @@ public class expeinces extends AppCompatActivity {
         });
 
 
+        final DatabaseReference database2 = FirebaseDatabase.getInstance().getReference("exp").child("alex").child("art");
+        database2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<proxyItem> proxyItems =new ArrayList<>();
+                ArrayList<String> keys =new ArrayList<>();
+                for(DataSnapshot snap:dataSnapshot.getChildren()){
+                    proxyItems.add(snap.getValue(proxyItem.class));
+                    keys.add(snap.getKey());
+
+                    Log.e("print",snap.toString());
+                }
+                showCatItems(proxyItems,database2,keys,"art");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("blablabla", "Failed to read value.", error.toException());
+            }
+        });
+
+
+        final DatabaseReference database3 = FirebaseDatabase.getInstance().getReference("exp").child("alex").child("sport");
+        database3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<proxyItem> proxyItems =new ArrayList<>();
+                ArrayList<String> keys =new ArrayList<>();
+                for(DataSnapshot snap:dataSnapshot.getChildren()){
+                    proxyItems.add(snap.getValue(proxyItem.class));
+                    keys.add(snap.getKey());
+
+                    Log.e("print",snap.toString());
+                }
+                showCatItems(proxyItems,database3,keys,"sport");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("blablabla", "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
         //loop in data
         //in each loop show its items
 
     }
 
-    private void showCatItems(x cat,DatabaseReference myRef) {
+    private void showCatItems(ArrayList<proxyItem> proxyItems , DatabaseReference myRef, final ArrayList<String> keys,String title) {
         CardView cardView=new CardView(getApplicationContext());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             cardView.setCardElevation(10.2f);cardView.setElevation(10.2f);
@@ -79,9 +139,7 @@ public class expeinces extends AppCompatActivity {
         LinearLayout linearLayout1=new LinearLayout(getApplicationContext());
         linearLayout1.setOrientation(LinearLayout.VERTICAL);
         TextView Title=new TextView(getApplicationContext());
-        Log.e("zzza",cat.getItems().length+"" +
-                "");
-        Title.setText(cat.getItems()[0].getTitle());
+        Title.setText(title);
         Title.setTypeface(null,Typeface.BOLD);
         Title.setTextSize(22);
         cardView.addView(linearLayout1);
@@ -95,55 +153,50 @@ public class expeinces extends AppCompatActivity {
         final LinearLayout ls=new LinearLayout(getApplicationContext());
         ls.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
 
-        final LinearLayout eachItem=new LinearLayout(getApplicationContext());
 
 
+for(int i=0;i<proxyItems.size();i++) {
+    final LinearLayout eachItem=new LinearLayout(getApplicationContext());
+    eachItem.setOrientation(LinearLayout.VERTICAL);
+    eachItem.setLayoutParams(new LinearLayout.LayoutParams(200, 260));
+    final ImageView imageView = new ImageView(getApplicationContext());
+    StorageReference storageRef = FirebaseStorage.getInstance().getReference(keys.get(i)+"0.jpg");
 
-                eachItem.setOrientation(LinearLayout.VERTICAL);
-                eachItem.setLayoutParams(new LinearLayout.LayoutParams(200,260));
-                ImageView imageView= new ImageView(getApplicationContext());
-                imageView.setImageResource(R.drawable.ronaldo);
-                imageView.setLayoutParams(new LinearLayout.LayoutParams(200,200));
-                TextView name=new TextView(getApplicationContext());
-                name.setText("sdf");
-                name.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,50));
-                eachItem.addView(imageView);
-                eachItem.addView(name);
-                ls.addView(eachItem);
+    final long ONE_MEGABYTE = 1024 * 1024;
+    storageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+        @Override
+        public void onSuccess(byte[] bytes) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            imageView.setImageBitmap(bitmap);
+        }
+    }).addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception exception) {
+            // Handle any errors
+        }
+    });
+
+    imageView.setLayoutParams(new LinearLayout.LayoutParams(200, 200));
+    TextView name = new TextView(getApplicationContext());
+    name.setText(proxyItems.get(i).getTitle());
+    name.setGravity(Gravity.CENTER);
+    name.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 50));
+    eachItem.addView(imageView);
+    eachItem.addView(name);
+    ls.addView(eachItem);
 
 
-
+}
         linearLayout1.addView(sv);
         linearLayout.addView(cardView);
         sv.addView(ls);
-        ViewGroup.MarginLayoutParams layoutParams1 =
-                (ViewGroup.MarginLayoutParams) eachItem.getLayoutParams();
-        layoutParams1.setMargins(0, 0, 5, 0);
-        eachItem.requestLayout();
+
         ViewGroup.MarginLayoutParams layoutParams2 =
                 (ViewGroup.MarginLayoutParams) cardView.getLayoutParams();
         layoutParams2.setMargins(0, 0, 0, 9);
         cardView.requestLayout();
     }
-    private static  class x {
-        public x() {
-        }
-        proxyItem[] items;
 
-        public proxyItem[] getItems() {
-            return items;
-        }
-
-        public void setItems(proxyItem[] items) {
-            this.items = items;
-        }
-
-
-
-
-
-
-    }
     private static  class proxyItem {
 
         String title,des,address;
